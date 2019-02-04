110f0c44060c632d972d60edaac74494d05820b3
Cedrick Mulumba MBUYAMBA <cmbuyamb@e4r9p3.wethinkcode.co.za>
2019-02-04 16:37:00 +0200

    camagru

---
+++ fragments/footer.php
@@ -0,0 +1,1 @@
+<div class="footer">2018 - cmbuyamb@wtc.co.za</div>
---
+++ setup/database.php
@@ -0,0 +1,7 @@
+<?php
+$DB_NAME = "camagru";
+$DB_DSN = "mysql:host=localhost:3306;dbname=".$DB_NAME;
+$DB_DSN_LIGHT = "mysql:host=localhost:3306";
+$DB_USER = "root";
+$DB_PASSWORD = "lemba1991";
+?>
